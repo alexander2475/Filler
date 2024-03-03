52c6cf08074b1fd026eb1ad0d9fd4eb2f2958a4b
alexander <alexander2475@icloud.com>
2024-03-02 19:10:47 -0700

    Players are no longer able to pick the same color. Fixed color not greying out after first move.

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 
-const colors = ['#FF595E', '#FF924C', '#FFCA3A', '#8AC926', '#1982C4', '#6A4C93'];
+const colors = ['#FEB202','#633DD7', '#D1383B','#238bf3','#2CA082', '#ED7F01'];
 
 let board =
+    [
+        ['x', 'y', 'x', 'x', 'x', 'x',],
+        ['x', 'x', 't', 'x', 'x', 'x',],
+        ['x', 'x', 'x', 'y', 'x', 'x',],
+        ['x', 'x', 'y', 'y', 'x', 'x',]
+    ];
+
+let player1Board =
+    [
+        ['x', 'y', 'x', 'x', 'x', 'x',],
+        ['x', 'x', 't', 'x', 'x', 'x',],
+        ['x', 'x', 'x', 'y', 'x', 'x',],
+        ['x', 'x', 'y', 'y', 'x', 'x',]
+    ];
+
+let player2Board =
     [
         ['x', 'y', 'x', 'x', 'x', 'x',],
         ['x', 'x', 't', 'x', 'x', 'x',],
@@ -17,8 +33,8 @@ for (let row of board){
 let turn = true;
 console.log("Is is player1's turn: " + turn);
 
-let player1SP;
-let player2SP;
+let player1Captured;
+let player2Captured;
 let player1Name;
 let player2Name;
 
@@ -31,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function() {
     for (let j = 0; j < colors.length; j++){
         const colorOpt = document.createElement('div');
         colorOpt.className = 'colorTiles';
-        colorOpt.textContent = j.toString();
+        colorOpt.textContent = " ";
         colorChoice.appendChild(colorOpt);
     }
 
@@ -42,18 +58,18 @@ document.addEventListener('DOMContentLoaded', function() {
             const tile = document.createElement('div');
             tile.className = 'tile';
             //tile.textContent = `Tile ${counter++}\n${row}`+`,${column}`;
-            tile.innerHTML = `Tile ${counter++}<br>${row},${column}`;
+            tile.innerHTML = `Tile ${counter++}<br>(${row},${column})`;
 
             board[row][column] = tile;
 
             gameArea.appendChild(tile);
 
             if (row === 0 && column === 5){
-                player1SP = board[row][column];
+                player1Captured = board[row][column];
             }
             // Assign player2 to bottom left corner
             else if (row === 3 && column === 0){
-                player2SP = board[row][column];
+                player2Captured = board[row][column];
             }
         }
     }
@@ -83,24 +99,51 @@ function setupColorsBar() {
 
 //FIXME: this function is not able to update the player title cards.
 function currentTurn(colorIndex){
-    //this.style.opacity = 0.5;
-    turn = !turn;
+
+    let players = document.getElementsByClassName('player-names');
+
     console.log("Is is player1's turn: " + turn);
+    greyColors();
 
     let color = colors[colorIndex];
 
     if (turn) {
+        player1Name.textContent = "-> Player 1 <-";
+        player2Name.textContent = "Player 2";
         player1Name.style.backgroundColor = color;
-        player1SP.style.backgroundColor = color;
+        player1Captured.style.backgroundColor = color;
+        greyColors();
+        turn = !turn;
     } else {
+        player2Name.textContent = "-> Player 2 <-";
+        player1Name.textContent = "Player 1";
         player2Name.style.backgroundColor = color;
-        player2SP.style.backgroundColor = color;
+        player2Captured.style.backgroundColor = color;
+        greyColors();
+        turn = !turn;
+    }
+}
+
+function greyColors(){
+
+    let colorTiles = document.getElementsByClassName('colorTiles');
+
+    //iterate through array for the color options and increment counter i.
+    for (let i = 0; i < colors.length; i++){
+        if (colorTiles[i].style.backgroundColor === player1Captured.style.backgroundColor || colorTiles[i].style.backgroundColor === player2Captured.style.backgroundColor){
+            colorTiles[i].style.pointerEvents = 'none';
+            colorTiles[i].style.opacity = .5;
+        } else {
+            colorTiles[i].style.pointerEvents = 'auto';
+            colorTiles[i].style.opacity = 1;
+        }
     }
 }
 
 //FIXME: this function is not assigning an initial color value to the player titles.
 function setupGame() {
     let playerNames = document.getElementsByClassName('player-names');
+    let title = document.getElementById('title');
 
     for (let column = 0; column < 6; column++) {
         for (let row = 0; row < 4; row++) {
@@ -111,6 +154,8 @@ function setupGame() {
         }
     }
 
+    greyColors();
+    playerNames[1].textContent = "-> Player 1 <-";
     playerNames[1].style.backgroundColor = board[0][5].style.backgroundColor;
     playerNames[2].style.backgroundColor = board[3][0].style.backgroundColor;
 }
